b9cabd26850990bf3defbdb525562062a7a1cf46
kikiwww <kili1154798056@gmail.com>
2025-08-22 14:46:47 +0800

    ADD: 异步下载的问题

--- app/utils/jm_downloader.py
+++ app/utils/jm_downloader.py
@@ -17,8 +17,9 @@ def download_and_zip(jm_id: int, config_path: str = JM_CONFIG_FILE) -> str:
     option = jmcomic.create_option_by_file(config_path)
 
     # 使用 option 对象来下载本子
-    download_album(jm_id, option)
-
+    data1 = download_album(jm_id, option)
+    logger.info(f"下载完真正的jm_id：{data1[0].id}")
+    jm_id = data1[0].id
     # 压缩下载的文件夹
     folder_to_zip = f'{jm_downloader.get("dir_rule.base_dir")}/{jm_id}'
     dest_dir = config.get('save.dest_dir')
--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import uvicorn
+from jmcomic import JmAlbumDetail
 
-from app.utils.yaml_config import config
-from app.init.app_initializer import AppInitializer
-from app.utils.logger_utils import logger
-# 创建初始化工具实例
-initializer = AppInitializer(base_dir=".", logger=logger)
+from app.utils.jm_downloader import download_and_zip
 
-# 已弃用 换成honcho start 命令启动
-if __name__ == "__main__":
-    initializer.initialize()
-    port = config.get("server.port")
-    uvicorn.run("app.fast.fast_app:server_app", host="127.0.0.1", port=port, reload=True)
+download_and_zip(422866)
+JmAlbumDetail
