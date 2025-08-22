9564d376b8e3b4832271076fa548ca46c7987367
kikiwww <kili1154798056@gmail.com>
2025-08-22 11:34:16 +0800

    ADD: 异步任务下载 使用缓存

--- app/fast/service/download_service.py
+++ app/fast/service/download_service.py
@@ -8,9 +8,10 @@ def download_jm_comic(jm_id: int):
     folder_path = config.get("save.dest_dir")
     cache = bool(config.get("save.cache"))
     file_path = os.path.join(folder_path, f"{jm_id}.zip")
-    logger.info(f"是否使用缓存: {cache and os.path.exists(file_path) and os.path.isfile(file_path)}")
+    use_cache = cache and os.path.exists(file_path) and os.path.isfile(file_path)
+    logger.info(f"是否使用缓存: {use_cache}")
     # 如果已经下载过文件 并且使用缓存
-    if cache and os.path.exists(file_path) and os.path.isfile(file_path):
+    if use_cache:
         return file_path
     else:
         data = download_and_zip(jm_id)
