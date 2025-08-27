bae0e216808347815bd1f2d49cddc95f9d178eea
kikiwww <kili1154798056@gmail.com>
2025-08-27 16:42:39 +0800

    ADD: 重试问题

--- app/celery/celery_worker.py
+++ app/celery/celery_worker.py
@@ -9,5 +9,5 @@ celery_app = Celery(
     "worker",
     broker=redis_url,  # Redis 作为消息队列
     backend=redis_end_url,  # 存储任务结果
-    include=["app.job"]
+    include=["app.job.download_job", "app.job.retry_job"]
 )
