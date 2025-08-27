85f80053029e5177fb59acdf47c764766fbaa8e5
kikiwww <kili1154798056@gmail.com>
2025-08-27 16:48:58 +0800

    FIX: 重试问题

--- app/celery/celery_worker.py
+++ app/celery/celery_worker.py
@@ -1,13 +1,23 @@
-from app.utils.logger_utils import logger
-from app.utils.yaml_config import config
 from celery import Celery
+from app.utils.yaml_config import config
 
+# Redis 配置
 redis_url = config.get("server.redis")
 redis_end_url = config.get("server.redis_end")
-logger.info(redis_url)
+
 celery_app = Celery(
     "worker",
-    broker=redis_url,  # Redis 作为消息队列
-    backend=redis_end_url,  # 存储任务结果
-    include=["app.job.download_job", "app.job.retry_job"]
+    broker=redis_url,                # 消息队列
+    backend=redis_end_url,           # 任务结果存储
+    include=[
+        "app.job.download_job",
+        "app.job.retry_job"
+    ] # 指向具体任务模块
+)
+
+# 可选：任务序列化配置
+celery_app.conf.update(
+    task_serializer='json',
+    result_serializer='json',
+    accept_content=['json'],
 )
