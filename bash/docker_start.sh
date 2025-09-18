6f10ca795b5e40ee9a53373814c15254a057eb14
kikiwww <kili1154798056@gmail.com>
2025-09-18 15:04:09 +0800

    ADD: docker sh脚本

--- bash/docker_start.sh
+++ bash/docker_start.sh
@@ -121,6 +121,6 @@ mkdir -p log
 
 # 启动 honcho
 log_info "启动 honcho 后台服务..."
-honcho start -c worker_download=$WORK_SIZE,worker_default=$DEFAULT_SIZE >> log/production.log 2>&1
 log_info "honcho 已后台启动，日志输出到 log/production.log"
+honcho start -c worker_download=$WORK_SIZE,worker_default=$DEFAULT_SIZE >> log/production.log 2>&1
 
